0ef15dcb9c9989ca137830b9acd769e69e5ffcae
Rafael Levi <rafaellevissa@hotmail.com>
2013-03-18 21:55:05 -0300

    Update Jogo.java
    add code

--- class/Jogo.java
+++ class/Jogo.java
@@ -1,14 +1,23 @@
-
 public class Jogo {
-	
-	private boolean status;
-
-	public boolean getStatus() {
-		return status;
-	}
-
-	public void setStatus(boolean status) {
-		this.status = status;
-	}
-
+    private boolean status = false;
+    public boolean getStatus() {
+            return status;
+    }
+    public void setStatus(boolean status) {
+            this.status = status;
+    }
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        Jogador Jogador1 = new Jogador();
+        Jogador Jogador2 = new Jogador();
+        Jogo oJogo = new Jogo();
+        System.out.println("Digite o nome do jogador 1");
+        String jname = in.nextLine(); 
+        Jogador1.setNome(jname); 
+        System.out.println("Digite o nome do jogador 2");
+        jname = in.nextLine();
+        Jogador2.setNome(jname);
+        oJogo.setStatus(true);
+        
+    }
 }
